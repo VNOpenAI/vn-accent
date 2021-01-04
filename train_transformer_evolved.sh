09eb988636415aac68f87606b9a525cca8b049a8
Viet Anh Nguyen <vietanhdev@gmail.com>
2021-01-04 14:10:01 +0700

    Update README

--- train_transformer_evolved.sh
+++ train_transformer_evolved.sh
@@ -2,5 +2,4 @@ python3 train.py 'data/tokenizer.h5' 'data/train' 'data/val' \
 --num_epochs 20 \
 --cuda \
 --learning_rate 0.0003 \
---model_name 'base_evolved' \
---weight_dir 'base_weight'
+--model_name 'transformer_evolved'
